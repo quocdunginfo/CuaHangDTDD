d7d4079453d02d137fadecc0f060a3da682473b7
quocdunginfo <quocdunginfo@gmail.com>
2013-11-10 02:08:53 +0700

    Đang làm trang hiển thị DTDD theo Hãng SX...
    Cơ bản gần xong
    Có phân trang, có class Pagination tự động tính toán offset,...

--- CuaHangDTDD/Controllers/FrontProductsController.cs
+++ CuaHangDTDD/Controllers/FrontProductsController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CuaHangDTDD.ModelController;
+using CuaHangDTDD.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,8 +16,31 @@ namespace CuaHangDTDD.Controllers
          * Có nút Add to Cart => redirect đến trang Sản phẩm chi tiết để chọn
          * SL và màu sắc cần đặt
          */
-        public ActionResult Index()
+        public ActionResult Index(int hangsx_id=0, int page=1)
         {
+            SanPhamController ctr = new SanPhamController();
+            HangSXController ctr_hangsx = new HangSXController();
+            HangSX hangsx = ctr_hangsx.get_by_id(hangsx_id);
+            List<HangSX> search_hangsx = ctr_hangsx.timkiem(hangsx_id.ToString(),"","1");
+            //check
+            if (hangsx==null || hangsx.active==false)
+            {
+                return RedirectToAction("Index","FrontHome");
+            }
+            //pagination
+            int max_item_per_page=4;//will get from setting
+            //pagination
+                Pagination pg = new Pagination();
+                pg.set_current_page(page);
+                pg.set_max_item_per_page(max_item_per_page);
+                pg.set_total_item(
+                    ctr.timkiem_count("", "", "", "", -1, -1, search_hangsx, "1")
+                );
+            pg.update();
+            ViewBag.SanPham_List = ctr.timkiem("","","","",-1,-1,search_hangsx,"1","id",true,pg.start_point,max_item_per_page);
+            ViewBag.pagination = pg;
+            ViewBag.HangSX_List = ctr_hangsx.timkiem("","","1");
+            ViewBag.hangsx = hangsx;
             return View();
         }
 
